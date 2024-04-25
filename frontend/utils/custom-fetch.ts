a0bc3550a48370328b0ccebb87b200a662407e65
rocktimsaikia <rocktimthedev@gmail.com>
2024-04-25 17:36:55 +0530

    refactor(frontend/utils): optimize header setup in customFetch function

--- frontend/utils/custom-fetch.ts
+++ frontend/utils/custom-fetch.ts
@@ -6,6 +6,7 @@ type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 type FetchOptions<T> = {
   method: Method;
   body?: T;
+  headers?: Record<string, string>;
 };
 
 export async function customFetch<T>(path: string, options?: FetchOptions<T>) {
@@ -22,32 +23,36 @@ export async function customFetch<T>(path: string, options?: FetchOptions<T>) {
 function buildConfig<T>(options?: FetchOptions<T>): RequestInit {
   const method = options?.method || "GET";
   const body = options?.body;
+  const headers = options?.headers;
   validateRequestBody(method, body);
+
+  const baseHeaders = {
+    ...headers,
+    ...(doesMethodRequireBody(method) && body
+      ? { "Content-Type": "application/json" }
+      : {}),
+  };
 
   return {
     method: method,
-    // "headers" and "body" are only required in the request
-    // for POST, PUT, and PATCH requests. Since the "body" is
-    // optional, we only include it if it exists.
-    ...(["POST", "PUT", "PATCH"].includes(method) && body
-      ? {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
-        }
+    headers: baseHeaders,
+    ...(doesMethodRequireBody(method) && body
+      ? { body: JSON.stringify(body) }
       : {}),
   };
 }
 
 function validateRequestBody<T>(method: Method, body?: T): void {
-  const doesMethodRequireBody = ["POST", "PUT", "PATCH"].includes(method);
-
-  if (!doesMethodRequireBody && body) {
+  if (!doesMethodRequireBody(method) && body) {
     throw new Error(`${method} requests should not have a body`);
   }
 
-  if (doesMethodRequireBody && !body) {
+  if (doesMethodRequireBody(method) && !body) {
     throw new Error(`Request body is required for ${method} requests`);
   }
+}
+
+// New function to determine if the method requires a body
+function doesMethodRequireBody(method: Method): boolean {
+  return ["POST", "PUT", "PATCH"].includes(method);
 }
